ebd012994692cc3d9bffaa4607574852ad0fec09
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-05-16 01:03:22 -0300

    Compilando e abrindo os relatórios 01 e 02 com JasperViewer

--- src/main/java/com/projeto/jasper/jdbc/JasperService.java
+++ src/main/java/com/projeto/jasper/jdbc/JasperService.java
@@ -1,10 +1,10 @@
 package com.projeto.jasper.jdbc;
 
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.view.JasperViewer;
 
 import java.io.InputStream;
+import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +14,17 @@ public class JasperService {
 
     public void addParams(String key, Object value) {
         this.params.put(key, value);
+    }
+
+    public void AbrirJasperViewer(String jrxml, Connection connection) {
+        JasperReport report = compilarJrxml(jrxml);
+        try {
+            JasperPrint print = JasperFillManager.fillReport(report, this.params, connection);
+            JasperViewer viewer = new JasperViewer(print);
+            viewer.setVisible(true);
+        } catch (JRException e) {
+            e.printStackTrace();
+        }
     }
 
     private JasperReport compilarJrxml(String arquivo) {
--- src/main/java/com/projeto/jasper/jdbc/Main.java
+++ src/main/java/com/projeto/jasper/jdbc/Main.java
@@ -1,9 +1,19 @@
 package com.projeto.jasper.jdbc;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        abrirJrxml("02");
+    }
 
+    private static void abrirJrxml(String numero) throws SQLException {
+        Connection connection = JdbcConnection.connection();
+        JasperService service = new JasperService();
+        service.AbrirJasperViewer("relatorios/jrxml/funcionarios-" + numero + ".jrxml", connection);
+        connection.close();
     }
 
 }
